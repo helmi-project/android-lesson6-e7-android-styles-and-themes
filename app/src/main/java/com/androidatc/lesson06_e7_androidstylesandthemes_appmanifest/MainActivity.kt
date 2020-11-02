f9dfc319b9c2e9152ca5f5f7470d6777cb591c7c
helmibelogix <helmi@belogix.com>
2020-11-02 11:36:34 +0700

    First Commit

---
+++ app/src/main/java/com/androidatc/lesson06_e7_androidstylesandthemes_appmanifest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidatc.lesson06_e7_androidstylesandthemes_appmanifest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
